1147325f41dcafcff73525b9c4f2c71f5f2f9439
KrouriB <brice.krouri@gmail.com>
2023-02-14 17:00:02 +0100

    foncton afficher equipe par pays avec css

--- Nation.php
+++ Nation.php
@@ -17,7 +17,16 @@ class Nation
         $this->$equipes[] = $uneEquipe;
     }
 
-    
+    public function afficherEquipe()
+    {
+        $display = "<div style='background-color:lightcoral;width:360px;height:360px;padding:40px;display:flex;flex-direction:column;justify-content:space-around'><strong>$this</strong><div style='display:flex;flex-direction:column;align-content: flex-start'>";
+        foreach($this->$equipes as $uneEquipe)
+        {
+            $display .= "<span>$uneEquipe</span>";
+        }
+        $display .= "</div></div><br><br>";
+        echo $display;
+    }
 
     public function __toString()
     {
