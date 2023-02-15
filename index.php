439f92ae8f5a8d2cbc912d4470cefacefee7ae8b
KrouriB <brice.krouri@gmail.com>
2023-02-15 12:13:21 +0100

    creation objet et test de leur methode

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
+<style>
+*{
+	margin: 1%;
+    padding: 0;
+    box-sizing: border-box;
+	font-family: "Gill Sans", sans-serif;
+}
+</style>
+
 <?php
 
 spl_autoload_register(function ($class_name) {
 	require_once $class_name . '.php';
 });
 
+$fr = new Nation ("France");
+$psg = new Equipe ("Paris Saint-Germain", 1970, $fr);
+$jHE = new Joueur ("Ekitike", "Hugo", "20-06-2002", $fr, $psg);
+$s2022HE = new Saison (2022, "en cours", $jHE, $psg);
+$fr->afficherEquipe();
+$jHE->afficherInfoJoueur();
+$psg->afficherInfoEquipe();
