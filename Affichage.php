13bbaec9e88354be252a7ec3327788598bcd0589
KrouriB <brice.krouri@gmail.com>
2023-02-28 09:44:09 +0100

    fonction pour afficher les objet des class

--- Affichage.php
+++ Affichage.php
@@ -15,20 +15,47 @@ class Affichage
 	}
 
 	public function ajouterEquipe(Equipe $uneEquipe)
-    {
-        $this->equipes[] = $uneEquipe;
-    }
+	{
+		$this->equipes[] = $uneEquipe;
+	}
 
 	public function ajouterNation(Nation $uneNation)
-    {
-        $this->nations[] = $uneNation;
-    }
+	{
+		$this->nations[] = $uneNation;
+	}
 
 	public function ajouterJoueur(Joueur $uneJoueur)
-    {
-        $this->joueurs[] = $uneJoueur;
-    }
+	{
+		$this->joueurs[] = $uneJoueur;
+	}
 
+	public function afficherEquipe()
+	{
+		$display = "<div style='display:flex; flex-direction:raw; flex-wrap:wrap; gap:20px'>";
+		foreach ($this->equipes as $uneEquipe) {
+			$display .= $uneEquipe->blocInfoEquipe();
+		}
+		$display .= "</div><br><br>";
+		echo $display;
+	}
+
+	public function afficherJoueur()
+	{
+		$display = "<div style='display:flex; flex-direction:raw; flex-wrap:wrap; gap:20px'>";
+		foreach ($this->joueurs as $uneJoueur) {
+			$display .= $uneJoueur->blocInfoJoueur();
+		}
+		$display .= "</div><br><br>";
+		echo $display;
+	}
+
+	public function afficherPays()
+	{
+		$display = "<div style='display:flex; flex-direction:raw; flex-wrap:wrap; gap:20px'>";
+		foreach ($this->nations as $unePays) {
+			$display .= $unePays->blocInfoPays();
+		}
+		$display .= "</div><br><br>";
+		echo $display;
+	}
 }
-
-?>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <style>
-*{
-	margin: 1%;
-    padding: 0;
-    box-sizing: border-box;
-	font-family: "Gill Sans", sans-serif;
-}
+	* {
+		margin: 1%;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: "Gill Sans", sans-serif;
+	}
 </style>
 
 <?php
@@ -14,14 +14,17 @@ spl_autoload_register(function ($class_name) {
 });
 
 $all = new Affichage();
-$fr = new Nation ("France", $all);
+$fr = new Nation("France", $all);
 $us = new Nation("Etats-Unis", $all);
-$psg = new Equipe ("Paris Saint-Germain", 1970, $fr, $all);
-$cb = new Equipe ("Chicago Bulls", 1996, $us, $all);
-$jHE = new Joueur ("Ekitike", "Hugo", "20-06-2002", $fr, $psg, $all);
-$jJR = new Joueur ("Joueur", "RandomAuUSA", "27-02-1999", $us, $cb, $all);
-$s2022HE = new Saison (2022, "en cours", $jHE, $psg);
-$s2022JR = new Saison (2018, 2021, $jJR, $cb);
-$fr->afficherEquipe();
-$jHE->afficherInfoJoueur();
-$psg->afficherInfoEquipe();
+$psg = new Equipe("Paris Saint-Germain", 1970, $fr, $all);
+$cb = new Equipe("Chicago Bulls", 1996, $us, $all);
+$jHE = new Joueur("Ekitike", "Hugo", "20-06-2002", $fr, $psg, $all);
+$jJR = new Joueur("Joueur", "RandomAuUSA", "27-02-1999", $us, $cb, $all);
+$s2022HE = new Saison(2022, "en cours", $jHE, $psg);
+$s2022JR = new Saison(2018, 2021, $jJR, $cb);
+// $fr->afficherInfoPays();
+// $jHE->afficherInfoJoueur();
+// $psg->afficherInfoEquipe();
+$all->afficherEquipe();
+$all->afficherJoueur();
+$all->afficherPays();
